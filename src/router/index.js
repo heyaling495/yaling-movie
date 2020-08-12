26cac4c8d314214e7fabf9c3ad1c399e3a07243d
“heyaling495 <1360462218@qq.com>
2020-08-12 22:27:38 +0800

    add createCompnents

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import movieRouter from './movie'
+import mineRouter from './mine'
+import cinemaRouter from './cinema'
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,  //路径
+  routes:[
+    movieRouter,
+    mineRouter,
+    cinemaRouter,
+    {
+      path:'/',
+      redirect:'/movie'
+    }
+  ]
+})
+
+export default router
